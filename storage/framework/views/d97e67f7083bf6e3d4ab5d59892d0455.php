272de16ffeb6a2d0978fe4e7154a3eb4aa0c90c5
Priyanshu6055 <pkrr6055224@gmail.com>
2026-09-10 15:23:17 +0530

    Bug fixes

--- storage/framework/views/d97e67f7083bf6e3d4ab5d59892d0455.php
+++
@@ -1,8 +0,0 @@
-<section class="cta-band reveal">
-    <h2>Start with the conversation that matches your objective.</h2>
-    <div>
-        <p>Whether you are raising capital, evaluating equity, investing, acquiring or exploring a strategic partnership, fill out our contact form to discuss your specific requirements directly with our team.</p>
-        <a class="btn" href="<?php echo e(route('inquiries.create', 'contact')); ?>">Go to Contact Form &rarr;</a>
-    </div>
-</section>
-<?php /**PATH C:\Users\DESIGN\Downloads\BIC_Laravel_Production_Source\resources\views/components/home/cta.blade.php ENDPATH**/ ?>
